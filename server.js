0414b631b6382e9931641aa48dc0af3f49fecbf5
Ben Rothman <brothman7000@gmail.com>
2019-01-16 16:14:28 +0100

    Cleaned up server.js code and added conditionals to run locally and in production on heroku

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-// const express = require('express')
-// const app = express()
-
-// app.get('/', function (req, res) {
-//     res.send('Hello World!')
-// })
-
-// app.listen(3000, function () {
-//     console.log('Example app listening on port 3000!')
-// })
-
 const express = require('express');
 const app = express();
 const opn = require('opn');
@@ -23,24 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (request, response) => {
     response.render('index', { weather: null, error: null });
 });
-
-// app.post('/', function (req, res) {
-//     let city = req.body.city;
-//     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-//     request(url, function (err, response, body) {
-//         if (err) {
-//             res.render('index', { weather: null, error: 'Error, please try again' });
-//         } else {
-//             let weather = JSON.parse(body)
-//             if (weather.main == undefined) {
-//                 res.render('index', { weather: null, error: 'Error, please try again' });
-//             } else {
-//                 let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-//                 res.render('index', { weather: weatherText, error: null });
-//             }
-//         }
-//     });
-// })
 
 app.post('/', (request1, response) => {
     const city = request1.body.city;
@@ -63,12 +34,13 @@ app.post('/', (request1, response) => {
     });
 });
 
-// app.listen(3000, () => {
-//     console.log('First Node app listening on port 3000!');
-//     opn('http://localhost:3000/');
-// });
+const env = process.env.NODE_ENV || 'dev';
 
 //process.env.PORT lets the port be set by Heroku
-app.listen(process.env.PORT, () => {
-    //
+//PORT will be undefined if run locally with node server.js
+app.listen(process.env.PORT || 3000, () => {
+    if (env == 'dev') {
+        console.log('First Node app listening on port 3000!');
+        opn('http://localhost:3000/');
+    }
 });
