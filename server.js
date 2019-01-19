d7c6c3e86dae5bb92f63e41369e5657e161461a7
Ben Rothman <brothman7000@gmail.com>
2019-01-19 17:06:51 +0100

    Allowed users to pick celsius or fahrenheit for temperature

--- server.js
+++ server.js
@@ -10,31 +10,36 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
-    response.render('index', { weather: null, error: null });
+    response.render('index', { weather: null, error: null, obj: "hi" });
 });
 
 app.post('/', (request1, response) => {
     const city = request1.body.city;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    const unit = request1.body.unit;
+    const system = (unit == 'celsius') ? "metric" : "imperial";
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${system}&appid=${apiKey}`;
+    const urlCelsius = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    console.log(request1.body)
     request(url, (error, weatherResponse, body) => {
         if (error) {
-            response.render('index', { weather: null, error: 'Error, please try again' });
+            response.render('index', { weather: null, error: 'Error, please try again'});
         }
         else {
             const weather = JSON.parse(body);
             if (weather.main == undefined) {
-                response.render('index', { weather: null, error: 'Error, please try again' });
+                response.render('index', { weather: null, error: 'Error, please try again'});
             }
             else {
-                const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                const weatherText = `It's ${weather.main.temp} degrees ${unit} in ${weather.name}!`;
                 response.render('index', { weather: weatherText, error: null });
-                 
             }
         }
     });
 });
 
-const env = process.env.NODE_ENV || 'dev';
+const env = process.env.NODE_ENV || 'dev'; 
 
 //process.env.PORT lets the port be set by Heroku
 //PORT will be undefined if run locally with node server.js
